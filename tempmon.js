cceb42fee10cc0b88068f3bae87ec2ccada2e80e
Bogdan Bucur <bogdanrbucur@gmail.com>
2021-09-14 07:39:18 +0000

    initial commit

---
+++ tempmon.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const nodemailer = require('nodemailer');
+const dotenv = require("dontenv").config();
+
+let systemEmail = process.env.SCRIPT_EMAIL_USER;
+let systemPass = process.env.SCRIPT_EMAIL_PASS;
+let alertedEmail = process.env.ALERTED_EMAIL;
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'systemalert9@gmail.com',
+    pass: 'notAstockpassword'
+  }
+});
+
+const mailOptions = {
+  from: 'systemalert9@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Raspberry Pi Temp. Alert',
+  text: 'CPU temp. above'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
