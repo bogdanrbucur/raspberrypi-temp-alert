feeb70c3041992971e1687bd7b2eff7a72a63967
Bogdan Bucur <bogdanrbucur@gmail.com>
2021-09-14 08:13:04 +0000

    update get temperature

--- tempmon.js
+++ tempmon.js
@@ -1,30 +1,46 @@
 const fs = require("fs");
-const nodemailer = require('nodemailer');
-const dotenv = require("dontenv").config();
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv").config();
 
 let systemEmail = process.env.SCRIPT_EMAIL_USER;
 let systemPass = process.env.SCRIPT_EMAIL_PASS;
 let alertedEmail = process.env.ALERTED_EMAIL;
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
     user: systemEmail,
-    pass: systemPass
-  }
+    pass: systemPass,
+  },
 });
 
 const mailOptions = {
   from: systemEmail,
   to: alertedEmail,
-  subject: 'Raspberry Pi Temp. Alert',
-  text: 'CPU temp. above'
+  subject: "Raspberry Pi Temp. Alert",
+  text: "CPU temp. above",
 };
 
-transporter.sendMail(mailOptions, function(error, info){
+transporter.sendMail(mailOptions, function (error, info) {
   if (error) {
     console.log(error);
   } else {
-    console.log('Email sent: ' + info.response);
+    console.log("Email sent: " + info.response);
   }
 });
+
+function getTemp() {
+  fs.readFile(
+    "/sys/class/thermal/thermal_zone0/temp",
+    "utf8",
+    function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(`${(parseFloat(data)/1000).toFixed(1)} C`);
+      return `${(parseFloat(data)/1000).toFixed(1)} C`;
+    }
+  );
+}
+
+getTemp();
